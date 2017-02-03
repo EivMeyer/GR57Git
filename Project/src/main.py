676798373ff915abb20fdb87d2627ca51a614c16
EivMeyer <vegardnittervestad@gmail.com>
2017-02-03 17:06:54 +0100

    Added server hierarchy

--- Project/src/main.py
+++ Project/src/main.py
@@ -1,9 +1,4 @@
 import network
 import sys
 
-mode = sys.argv[1]
-
-if (mode == "server"):
-	network.server()
-elif (mode == "klient"):
-	network.client()
+network.tcp_connect()
--- Project/src/network.py
+++ Project/src/network.py
@@ -2,9 +2,23 @@ import socket
 import time
 import threading
 
-PORT = 8103
+SERVER_HIERARCHY = [
+	'129.241.187.159',
+	'129.241.187.158',
+	'129.241.187.161',
+	'129.241.187.147'
+]
+
+PORT = 8114
 connections = {}
 lock 		= threading.Lock()
+
+def get_local_ip():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# Connecting to default gateway using UDP
+	s.connect(('129.241.187.1', 0))
+	local_ip_address = s.getsockname()[0]
+	return local_ip_address
 
 def tcp_chat():
 	# Only for debug purposes
@@ -14,7 +28,7 @@ def tcp_chat():
 def tcp_receive(address):
 	global connections
 	while (True):
-		print("Listening for messages from " + str(address))
+		#print("Listening for messages from " + str(address))
 		connection = connections[address]
 		buf = connection.recv(64)
 		if (len(buf) > 0):
@@ -37,6 +51,26 @@ def tcp_connection_listener(server_socket):
 		print(str(address) + ' connected to the server')
 		t = threading.Thread(target = tcp_receive, 				args = ([address]))
 		t.start()
+
+def tcp_connect():
+	local_ip = get_local_ip()
+
+	global SERVER_HIERARCHY
+	for ip in SERVER_HIERARCHY:
+		if (ip == local_ip):
+			server()
+			break
+		else:
+			try:
+				connect(ip)
+				print("Succesfully connected to " + str(ip))
+				break
+			except Exception as e:
+				print(str(ip) + " is not reachable")
+				pass
+
+
+
 def server():
 	global PORT
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,12 +88,13 @@ def server():
 	print("Server listening on " + str(PORT))
 	
 
-def client():
-	server_ip = '129.241.187.159'
+def client(server_ip):
 	global connections
 	global PORT
 
 	clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Will throw an error if server isn't available
 	clientsocket.connect((server_ip, PORT))
 
 	connections[server_ip] = clientsocket
