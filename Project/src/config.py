2f7dcc2a713c5cfabd6941a25100339db4cdfe75
bernthulsker <jon_eirik_@hotmail.com>
2017-03-10 23:26:04 +0100

    Minor update

--- Project/src/config.py
+++ Project/src/config.py
@@ -3,10 +3,11 @@ N_BUTTONS 	= 3
 
 SERVER_HIERARCHY = [
 	#'10.22.64.233', # Eivind Laptop
-	'129.241.187.157',
-	'129.241.187.152', #Labplass 13
-	'129.241.187.144', #Labplass 12
-	'129.241.187.142', #Labplass 14
+	'129.241.187.157', # Labplass 19
+	'129.241.187.153', # Labplass 21
+	'129.241.187.152', # Labplass 13
+	'129.241.187.144', # Labplass 12
+	'129.241.187.142', # Labplass 14
 	'129.241.187.149', # Labplass 2
 	'129.241.206.238', # Kattelab 1
 	'129.241.206.223', #haakon kattelab
